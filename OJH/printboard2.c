c532fd53a7d9d3f9098fdeab33f2d4e2559e7aba
joon-hyuk848 <74858961+joon-hyuk848@users.noreply.github.com>
2020-11-25 17:24:25 +0900

    Create printboard2.c

---
+++ OJH/printboard2.c
@@ -0,0 +1,118 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+#define WHITE 7
+#define BLUE 1
+#define GREEN 2
+#define RED 4
+#define YELLOW 6
+
+struct board {
+	char name[16];
+	int num;
+	int build;
+	int value;
+	int pass_value;
+	char player;
+	int sell_value;
+	struct board* get_player;
+};
+
+void print_board(void);
+void printcolor(int color);
+
+int main(void) {
+
+	//y축 21칸 x축 81칸
+	print_board();
+	// 나중에 플레이어 위치, 땅값 구현할것.
+}
+
+void print_board(void) {
+	char boardpan[31][160] = {
+	"┏    18    ┳    19    ┳    20    ┳    21    ┳    22    ┳    23    ┳    24    ┳    25    ┳    26    ┳    27    ┓",
+	"┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃",
+	"  000000000  000000000  000000000  000000000  000000000  000000000  000000000  000000000  000000000    price   ",
+	"┠    17    ╋  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ╋    28    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠    16    ┫                                                                                       ┠    29    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠    15    ┫                                                                                       ┠    30    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠    14    ┫                                                                                       ┠    31    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠    13    ┫                                                                                       ┠    32    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠    12    ┫                                                                                       ┠    33    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠    11    ┫                                                                                       ┠    34    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠    10    ┫                                                                                       ┠    35    ┫",
+	"┃  country ┃                                                                                       ┃  country ┃",
+	"  000000000                                                                                          000000000 ",
+	"┠     9    ╋     8    ┳     7    ┳     6    ┳     5    ┳     4    ┳     3    ┳     2    ┳     1    ╋     0    ┫",
+	"┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃  country ┃",
+	"  000000000  000000000  000000000  000000000  000000000  000000000  000000000  000000000  000000000    price   ",
+	"┗  ─   ─   ┸  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┻  ─   ─   ┛"
+	};
+	for (int i = 0; i < 30; i = i + 3)
+	{
+		printf("%s\n", boardpan[i]);
+		if (i == 0)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				printf("   "); printcolor(BLUE); printf("%c ", '*'); printcolor(RED); printf("%c ", '*'); printcolor(GREEN); printf("%c ", '*'); printcolor(YELLOW); printf("%c ", '*'); printcolor(WHITE);
+			}
+
+		}
+		else if (i > 0 && i < 27)
+		{
+			printf("   "); printcolor(BLUE); printf("%c ", '*'); printcolor(RED); printf("%c ", '*'); printcolor(GREEN); printf("%c ", '*'); printcolor(YELLOW); printf("%c ", '*'); printcolor(WHITE);
+			printf("                                                                                        ");
+			printf("   "); printcolor(BLUE); printf("%c ", '*'); printcolor(RED); printf("%c ", '*'); printcolor(GREEN); printf("%c ", '*'); printcolor(YELLOW); printf("%c ", '*'); printcolor(WHITE);
+		}
+		else
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				printf("   "); printcolor(BLUE); printf("%c ", '*'); printcolor(RED); printf("%c ", '*'); printcolor(GREEN); printf("%c ", '*'); printcolor(YELLOW); printf("%c ", '*'); printcolor(WHITE);
+			}
+		}
+		printf("\n");
+		printf("%s\n", boardpan[i + 1]);
+		printf("%s\n", boardpan[i + 2]);
+	}
+	printf("%s\n", boardpan[30]);
+
+}
+
+void printcolor(int color) {
+	if (color == WHITE)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
+	}
+	else if (color == BLUE)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLUE);
+	}
+	else if (color == GREEN)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), GREEN);
+	}
+	else if (color == RED)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), RED);
+	}
+	else if (color == YELLOW)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), YELLOW);
+	}
+}
